eeffd0cbce753f7955e7924ba717aa782c361c4a
ngenohkevin <ngenohkevin19@gmail.com>
2021-11-01 15:10:40 +0300

    updated server

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/ngenohkevin/flock_manager/app"
 	"github.com/ngenohkevin/flock_manager/database"
 	"log"
@@ -16,11 +18,14 @@ func main() {
 	application := &app.App{}
 	application.Initialize(config)
 
-	log.Println("Starting server")
-
+	err := godotenv.Load()
+	if err != nil {
+		return 
+	}
+	port := os.Getenv("PORT")
 
 	srv := &http.Server{
-		Addr:         os.Getenv("PORT"),
+		Addr:         fmt.Sprintf("%s:%s","0.0.0.0",port),
 		Handler:      application.SetRouters(),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
