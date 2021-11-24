ec2efbaee1c17315555b16799c958cf980c0af11
ngenohkevin <ngenohkevin19@gmail.com>
2021-11-24 16:05:06 +0300

    added middleware

---
+++ app/middleware.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+	"net/http"
+)
+
+func (a *App) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		next.ServeHTTP(w, r)
+	})
+}
