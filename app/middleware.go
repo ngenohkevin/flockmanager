02eb1d1141f89175e84c2aec5f9a8ba6042c6696
ngenohkevin <ngenohkevin19@gmail.com>
2021-12-12 21:27:55 +0300

    modified

--- app/middleware.go
+++ app/middleware.go
@@ -6,10 +6,6 @@ import (
 
 func (a *App) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers:", "Origin, Content-Type, X-Auth-Token, Authorization")
-		w.Header().Set("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
 	})
 }
